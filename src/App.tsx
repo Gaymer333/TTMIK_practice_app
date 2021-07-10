ddc4dbdac1b912a82bd8c733a38a78937c2695aa
Gaymer333 <elflover333@gmail.com>
2021-07-10 10:30:25 +0200

    Added some new styling lib, and start making a card.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { Button, Card, CardActions, CardContent } from '@material-ui/core';
 import './App.scss';
 
 function App() {
 
-  return <p>Heyya</p>
+  return <>
+  
+    <Card id="mainCard">
+        <CardContent>
+          <p color="textSecondary">
+            Word of the Day
+          </p>
+          <h5>
+            test
+          </h5>
+          <p color="textSecondary">
+            adjective
+          </p>
+          <p>
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </p>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+  </>
 }
 
 export default App;
